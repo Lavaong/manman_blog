7106f62f3a65ee65712a7618c2e63c0209ed5fff
limanman <litlb@dcits.com>
2020-06-08 11:54:56 +0800

    初始化项目

---
+++ frontend/docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title : 'Sky Dragon0',
+    description: 'just a blog for record something',
+    theme: 'vdoing',
+    themeConfig: {
+        sidebar: 'structuring',
+        categoryText:'零零总总',
+        bodyBgImg: [
+
+        ]
+    }
+}
---
+++ frontend/docs/index.md
@@ -0,0 +1,11 @@
+---
+ title: 网站初始化
+ description: 记录一些东西，弃用hexo
+ date: 2020-06-08 11:12:36
+ categories:
+   - vue
+   - 前端 
+ tags: 
+   - 搭建
+---
+    
