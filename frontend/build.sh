702febcd0d0b6d87d39fc63f0155a8b9a3bc73f3
limanman <litlb@dcits.com>
2020-06-09 20:45:27 +0800

    网站部署到自有云服务器

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+
+
 yarn add -D vuepress
 nohup yarn docs:build &
 #停止60s等待构建完成
-t1=`date -d "$date1" +%s`
-t2=`date -d "$date2" +%s`
-i=$(($ti2 - $ti1 ))
+t1=`date +%s`
+t2=`date +%s`
+i=$(( $t2 - $t1 ))
 while [[ "$i" -ne "60"  ]]
 do
-    ti2=`date +%s`
-	  i=$(($ti2 - $ti1 ))
+    t2=`date +%s`
+	  i=$(( $t2 - $t1 ))
 done
 #重命名
 mv ./docs/.vuepress/dist ./docs/.vuepress/blog
